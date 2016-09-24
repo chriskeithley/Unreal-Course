b9c5fcbf2ab7c2e1fff85d34a59de89e40783761
Chris Keithley <chris.keithley@gmail.com>
2016-09-24 16:29:04 -0700

    DoorCreation

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,28 @@
+// Copyright Christopher Keithley 2016
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values
+AOpenDoor::AOpenDoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOpenDoor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
